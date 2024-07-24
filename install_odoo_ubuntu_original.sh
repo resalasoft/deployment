95bdfe873e82a14aa00389b488f222757bf9c074
ResalaSoft <resalasoft@users.noreply.github.com>
2024-07-24 19:14:51 +0300

    Update install_odoo_ubuntu_original.sh

--- install_odoo_ubuntu_original.sh
+++ install_odoo_ubuntu_original.sh
@@ -17,14 +17,14 @@
 # ./install_odoo_ubuntu.sh
 ################################################################################
 
-OE_USER="odoo4"
+OE_USER="odoo5"
 OE_HOME="/opt/$OE_USER"
 OE_HOME_EXT="/opt/$OE_USER/${OE_USER}-server"
 # The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 # Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="False"
 # Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-OE_PORT="8064"
+OE_PORT="8069"
 # Choose the Odoo version which you want to install. For example: 16.0, 15.0 or 14.0. When using 'master' the master version will be installed.
 # IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 14.0
 OE_VERSION="17.0"
@@ -40,7 +40,7 @@ OE_CONFIG="${OE_USER}-server"
 # Set the website name
 WEBSITE_NAME="odoo4.resalasoft.com"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-LONGPOLLING_PORT="8074"
+LONGPOLLING_PORT="8072"
 # Set to "True" to install certbot and have ssl enabled, "False" to use http
 ENABLE_SSL="True"
 # Provide Email to register ssl certificate
