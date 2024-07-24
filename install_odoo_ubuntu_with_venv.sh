c8b38c9d93112e104b1efd8f0041e896ef65f891
ResalaSoft <resalasoft@users.noreply.github.com>
2024-07-24 21:37:23 +0300

    Update install_odoo_ubuntu_with_venv.sh

--- install_odoo_ubuntu_with_venv.sh
+++ install_odoo_ubuntu_with_venv.sh
@@ -39,7 +39,7 @@ OE_SUPERADMIN="admin"
 GENERATE_RANDOM_PASSWORD="True"
 OE_CONFIG="conf-${OE_USER}"
 # Set the website name
-WEBSITE_NAME="test3.resalasoft.com"
+WEBSITE_NAME="test.resalasoft.com"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 LONGPOLLING_PORT="8072"
 # Set to "True" to install certbot and have ssl enabled, "False" to use http
