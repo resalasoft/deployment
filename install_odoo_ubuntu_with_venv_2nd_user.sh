73a647d3b1e1bf3a070cb4349beb1f6b34ad94be
ResalaSoft <resalasoft@users.noreply.github.com>
2024-07-25 02:49:00 +0300

    Update install_odoo_ubuntu_with_venv_2nd_user.sh

--- install_odoo_ubuntu_with_venv_2nd_user.sh
+++ install_odoo_ubuntu_with_venv_2nd_user.sh
@@ -364,7 +364,6 @@ fi
 #--------------------------------------------------
 if [ $INSTALL_NGINX = "True" ] && [ $ENABLE_SSL = "True" ]  && [ $WEBSITE_NAME != "example.com" ];then
   sudo apt-get remove certbot
-  sudo apt install snapd
   sudo snap install core
   sudo snap refresh core
   sudo snap install --classic certbot
