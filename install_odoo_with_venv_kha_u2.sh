9dce247a6019ab352142b2be470aea522d8cdd17
ResalaSoft <resalasoft@users.noreply.github.com>
2024-10-09 19:45:53 +0300

    Update install_odoo_with_venv_kha_u2.sh

--- install_odoo_with_venv_kha_u2.sh
+++ install_odoo_with_venv_kha_u2.sh
@@ -171,10 +171,7 @@ sudo chown -R $OE_USER:$OE_USER /home/$OE_USER
 # VENV
 #-------------------------
 echo -e "\n---- Setup python virtual environment ----"
-# sudo apt install python3-pip
-# # apt --fix-broken install
-# sudo apt install python3-pip
-# sudo pip3 install virtualenv --upgrade
+sudo pip3 install virtualenv
 cd $OE_HOME/
 virtualenv $OE_HOME_VENV
 source "$OE_HOME_VENV/bin/activate"
